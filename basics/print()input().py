0ade9daebaa7b7a06013d8c14c9e2496b8942dea
jojuaswand <jojuaswand10@gmail.com>
2022-05-03 14:07:06 +0530

    python basics

---
+++ basics/print()input().py
@@ -0,0 +1,44 @@
+# print("hi")
+# print(x) #NameError: name 'x' is not defined #we have not assigned value to x
+# x = 5
+# print(x)
+# y = 6
+# print(y)
+# print(x, y) #separator has an default value and it is space " "
+# print(x, y, sep="X") #separator is sep , it is separating values
+# print(x, y, sep="-")
+# print(x, sep="-") #separtor works only when there are more than 1 values
+#separator is based on values
+# print("hi") #by default end is having new line "\n"
+# print("bye", end="#") #ending the print statement with given value
+# #end is based on print()
+# print("hi")
+
+# print("hi")
+# print("hi", "world", sep="&", end="-")
+# print("#she", "sells sea ", "shells on the sea shore", sep="-", end="#")
+# print(sep=" ", "hello", "world")
+
+
+#input()
+# print(input()) #its taking user input and giving it as value
+# print("write your name: ", end=" ")
+# print(input())
+# print("write your name: ", input(), end=" ")
+# print(input("write your name: "))
+
+
+# print(input("1st value : "), input("2nd value: "), sep="$")
+# x = input("1st value: ")
+# y = input("2nd value: ")
+# print(x, y, sep="$")
+
+# print(input()) #by default input is taking string and returning the same
+
+# print("hi", end="#")
+# print("hello", "world", sep="&")
+# print("write name:", end=" ")
+# print(input())
+# print("write your name:", input())
+# print(input("write name: "))
+print(input)
