80f51a559772ea6962352dcb2c2df041eb927612
jojuaswand <jojuaswand10@gmail.com>
2022-05-04 16:10:44 +0530

    if

---
+++ if_.py
@@ -0,0 +1,101 @@
+# conditional statements
+#  == equals
+#  != not equals
+#  > greater than
+#  < lesser than
+#  >= greater than or equal to
+#  <= lesser than or equal to
+
+#
+# a = 10
+# b = 20
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a <= b)
+# print(a >= b)
+
+a = 200
+b = 500
+book = 350
+# if book == 200:
+#  print("bought")
+# else:
+#  print("not buying")
+# if book > 200:
+#  a = book
+#  book = book * 50 / 100
+#  a = a * 50 / 100
+#  if book == a:
+#   print("buy")
+
+# # s = "hello world"
+# s = "helloworld"
+# if s.find(" ") != -1:
+#  print("space is there")
+# else:
+#  print("no space")
+
+# a = "hello"
+# a = "even"
+# a = "qwertyuiop[]asdfghjkl;'zxcvbnm,./ 1234567890-!@#$%^&*()_+`~"
+# if len(a) % 2 == 0:
+#  print(a)
+# else:
+#  print(a[::-1])
+
+# print(range) class
+# print(range(50)) end value is mandatory
+# print(range(1, 50)) returns until end value
+# print(list(range(10)))
+
+# a = list(range(51))
+# if len(a) % 2 != 0:
+#  a.append(49)
+#  print("element added")
+# else:
+#  print(a)
+
+#
+# a = 10
+# b = 12
+# a = 11
+# a = 12
+# if a != b:
+#  if a % 2 == 0:
+#   print("a is even")
+#  else:
+#   print("a is odd")
+# else:
+#  print(b)
+#
+# a = 3
+# b = 4
+# if a == b:
+#  print(a)
+#  if isinstance(a, int):
+#   print(a * 2)
+# elif isinstance(a, int):
+#  print(a * 2)
+# else:
+#  print(b)
+
+# a = "malayalam"
+# a = "Tamil"
+# a = "mom"
+# if a == a[::-1]:
+#  print(f"{a} is a palindrome")
+# else:
+#  print(f"{a} is not a palindrome")
+
+# find the greatest number
+a = 50
+b = 30
+c = 20
+if a > b and a > c:
+ print(f"{a} is greater than {b} and {c}")
+elif b > c:
+ print(f"{b} is greater than both {a} and {c}")
+else:
+ print(f"{c} is greater than {a} and {b}")
